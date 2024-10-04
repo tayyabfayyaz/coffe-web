ed7358ac9b8c816d7d1dc2e238e0d6d47bd48dba
Tayyab Fayyaz <160423815+tayyabfayyaz@users.noreply.github.com>
2024-10-04 07:01:35 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,15 +29,7 @@ export default function RootLayout({
 
     
     <html lang="en">
-      <Head>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <script src="https://kit.fontawesome.com/f7659a3732.js" crossOrigin="anonymous"></script>
-      <link
-          rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-          />
-      </Head>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
